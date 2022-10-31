d005d946b0d3121e014ada4138ee5d53df49d5ea
trustpizza <maxelolsson@gmail.com>
2022-10-31 15:04:41 -0500

    finish king

---
+++ pieces/king.rb
@@ -0,0 +1,20 @@
+class King < Piece
+  include Stepable
+  
+  def to_s
+    color == :black ? "♚" : "♔"
+  end
+
+  def move_dirs
+    [
+      [0, 1],
+      [1, 1],
+      [1, 0],      
+      [0, -1],
+      [1, -1],
+      [-1, 1],
+      [-1, -1],
+      [-1, 0],
+    ]
+  end
+end
