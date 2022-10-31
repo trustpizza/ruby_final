8f67764c86a2226699e00d2767acc014db94b3da
trustpizza <maxelolsson@gmail.com>
2022-10-31 15:05:30 -0500

    Finish queen

---
+++ pieces/queen.rb
@@ -0,0 +1,20 @@
+class Queen  < Piece
+  include Slideable
+
+  def to_s
+    color == :black ? "♛" : "♕"
+  end
+
+  def move_dirs
+    [
+      [0, 1],  
+      [0, -1], 
+      [1, 0],
+      [-1, 0],
+      [1, 1],
+      [1, -1],
+      [-1, 1],
+      [-1, -1],
+    ]
+  end
+end
